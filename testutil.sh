87f37f655b4ffe8f4fc29999dc62985794a550ee
Alain Helfesntein <alain.helfenstein@semantiker.ch>
2022-05-21 12:46:14 +0200

    deploy testutil.sh no fails

--- testutil.sh
+++ testutil.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+rm -f .temp
+
 echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX01 >> .temp
 docker exec $(docker ps -q) test/base_test.sh >> .temp
 echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX02 >> .temp
 
 cat .temp
+
+
 echo "x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x"
-grep "test fail" .temp | wc -l >> .temp
+numberOfFails=$(grep "test fail" .temp | wc -l)
+echo "number of fails = $numberOfFails"
