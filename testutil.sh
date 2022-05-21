9d98d501668ca333ca929f63db884e55e07714e0
Alain Helfesntein <alain.helfenstein@semantiker.ch>
2022-05-21 12:38:52 +0200

    deploy testutil.sh cat

--- testutil.sh
+++ testutil.sh
@@ -2,8 +2,7 @@
 echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX01 >> .temp
 docker exec $(docker ps -q) test/base_test.sh >> .temp
 echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX02 >> .temp
-docker exec $(docker ps -q) test/base_test.sh | grep "test fail" | wc -l >> .temp
-echo YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY03 >> .temp
 
-echo .temp
+cat .temp
 echo "x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x"
+grep "test fail" .temp | wc -l >> .temp
