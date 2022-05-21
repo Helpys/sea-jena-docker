8a0bb19af6d47e8223b7fab057aa5d47b5c4784f
Alain Helfesntein <alain.helfenstein@semantiker.ch>
2022-05-21 13:47:40 +0200

    select test 1

--- test/fuseki_test.sh
+++ test/fuseki_test.sh
@@ -5,5 +5,6 @@ docker exec $(docker ps -q) bin/s-put http://localhost:3030/example/data default
 
 a=$(docker exec $(docker ps -q) bin/s-query \
 --service http://localhost:3030/example/query \
-'SELECT * {<http://example.org/#green-goblin> <http://xmlns.com/foaf/0.1/name> ?o}' | grep -o "Green Goblin")\
+'SELECT * {<http://example.org/#green-goblin> <http://xmlns.com/foaf/0.1/name> ?o}'\
+ | grep -o "Green Goblin")\
  && ./test/sea_test.sh "Green Goblin" $a
