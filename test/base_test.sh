ffd005e6ad96134ee61d157989a690c2ff90e2d6
Alain Helfesntein <alain.helfenstein@semantiker.ch>
2022-05-21 08:09:58 +0200

    deploy test OKIDOKI 3

--- test/base_test.sh
+++ test/base_test.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 echo HelloWorld
+docker exec $(docker ps -q) ruby -v
+docker exec $(docker ps -q) ls -lat
+docker exec $(docker ps -q) echo HELLO-WORLD
+docker exec $(docker ps -q) ls -lat test
+docker exec $(docker ps -q) ls -lat bin
+docker exec $(docker ps -q) ls -lat configuration
 test -f && echo "ok" || echo "test fail"
