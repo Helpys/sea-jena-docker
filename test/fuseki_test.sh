83cfae430b799f0b11fa24d3c63a55f6c5a5d4fd
Alain Helfesntein <alain.helfenstein@semantiker.ch>
2022-05-21 16:44:47 +0200

    load normal

--- test/fuseki_test.sh
+++ test/fuseki_test.sh
@@ -15,27 +15,24 @@ sparql="@base <http://example.org/> .
 
 SELECT * {<http://example.org/#green-goblin> <http://xmlns.com/foaf/0.1/name> ?o}"
 
-a=$(docker exec $(docker ps -q) bin/s-query \
---service http://localhost:3030/example/query \
-$sparql\
- | grep -o "Green Goblin")
+a=$(docker exec $(docker ps -q) bin/s-query --service http://localhost:3030/example/query $sparql | grep -o "Green Goblin")
 
  ./test/sea_test.sh "simply select test" "Green Goblin" $a
 
 #
 # "rdf & sparql star select test"
 #
-echo "load rdf star turtle example"
-docker exec $(docker ps -q) bin/s-put http://localhost:3030/example/data default test/rdf_star_example.ttl
-parql="PREFIX : <http://www.example.org/>
-
-SELECT ?claimer WHERE {
-   << :employee38 ?property ?value >> :accordingTo ?claimer
-}"
-
-a=$(docker exec $(docker ps -q) bin/s-query \
---service http://localhost:3030/example/query \
-$sparql\
- | grep -o "Green Goblin")
-
- ./test/sea_test.sh "sparql star select test" "Green Goblin" $a
+# echo "load rdf star turtle example"
+# docker exec $(docker ps -q) bin/s-put http://localhost:3030/example/data default test/rdf_star_example.ttl
+# sparql="PREFIX : <http://www.example.org/>
+#
+# SELECT ?claimer WHERE {
+#    << :employee38 ?property ?value >> :accordingTo ?claimer
+# }"
+#
+# a=$(docker exec $(docker ps -q) bin/s-query \
+# --service http://localhost:3030/example/query \
+# $sparql\
+#  | grep -o "Green Goblin")
+#
+#  ./test/sea_test.sh "sparql star select test" "Green Goblin" $a
