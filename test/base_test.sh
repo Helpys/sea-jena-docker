2781c6fc4a961ff4c84bad61399101ceb93506c0
Alain Helfesntein <alain.helfenstein@semantiker.ch>
2022-05-21 12:58:52 +0200

    deploy testutil.sh DO 3 fails

--- test/base_test.sh
+++ test/base_test.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 echo HelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorld
+docker exec $(docker ps -q) bin/s-put http://localhost:3030/example/data default test/turtle_example.ttl
 ruby -v
 ls -lat
-sea_test "aaa" "aaa"
-sea_test "aaa" "aaa"
-sea_test "aaa" "aaa"
-sea_test "aaa" "aaa"
-sea_test "aaa" "bbb"
-sea_test "aaa" "aaa"
-sea_test "aaa" "aaa"
+./sea_test "aaa" "aaa"
+./sea_test "aaa" "aaa"
+./sea_test "aaa" "aaa"
+./sea_test "aaa" "fff"
+./sea_test "aaa" "bbb"
+./sea_test "aaa" "ccc"
+./sea_test "aaa" "aaa"
