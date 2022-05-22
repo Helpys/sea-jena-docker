e2f546f6e62b06678b6d9ae650a01678d0fa57f2
Alain Helfesntein <alain.helfenstein@semantiker.ch>
2022-05-22 13:40:52 +0200

    load normal 22

--- test/fuseki_test.sh
+++ test/fuseki_test.sh
@@ -3,12 +3,12 @@ echo "---------------------------------"
 echo "test file: ($0)"
 echo "---------------------------------"
 bin/s-put http://localhost:3030/example/data default test/rdf_star_example.ttl
-sparql='"PREFIX : <http://www.example.org/>
+sparql="PREFIX : <http://www.example.org/>
 
 SELECT ?claimer WHERE {
    << :employee38 ?property ?value >> :accordingTo ?claimer
-}"'
-bin/s-query --service http://localhost:3030/example/query $sparql
+}"
+bin/s-query --service http://localhost:3030/example/query "$sparql"
 echo "----------"
 #
 # "simply select test"
