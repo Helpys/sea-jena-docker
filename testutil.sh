77938d61bb50a8a0318fd644f6510af93b88a078
Alain Helfesntein <alain.helfenstein@semantiker.ch>
2022-05-22 09:08:34 +0200

    load normal 14

--- testutil.sh
+++ testutil.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
-rm -f .temp
+echo "docker container 2"
+docker ps -a
+docker exec $(docker ps -q) bin/s-query --service http://localhost:3030/example/query 'SELECT * {?s ?p ?o}'
 
-echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX01 >> .temp
-docker exec $(docker ps -q) test/base_test.sh >> .temp
-docker exec $(docker ps -q) test/fuseki_test.sh >> .temp
-echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX02 >> .temp
-
-cat .temp
-
-echo "ruby testutil.sh"
-docker exec $(docker ps -q) ruby -v
-
-echo "x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x"
-numberOfFails=$(grep "test fail" .temp | wc -l)
-if [ $numberOfFails = 0 ]
-then
-  echo "Test OK"
-else
-  echo "$numberOfFails tests fails"
-  exit 1
-fi
+# rm -f .temp
+#
+# echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX01 >> .temp
+# docker exec $(docker ps -q) test/base_test.sh >> .temp
+# docker exec $(docker ps -q) test/fuseki_test.sh >> .temp
+# echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX02 >> .temp
+#
+# cat .temp
+#
+# echo "ruby testutil.sh"
+# docker exec $(docker ps -q) ruby -v
+#
+# echo "x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x"
+# numberOfFails=$(grep "test fail" .temp | wc -l)
+# if [ $numberOfFails = 0 ]
+# then
+#   echo "Test OK"
+# else
+#   echo "$numberOfFails tests fails"
+#   exit 1
+# fi
