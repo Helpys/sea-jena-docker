9ac06116eeebff5b313dbfdb7f61678835a59aa8
Alain Helfesntein <alain.helfenstein@semantiker.ch>
2022-05-22 13:52:09 +0200

    load normal 24

--- test/base_test.sh
+++ test/base_test.sh
@@ -6,13 +6,8 @@ bin/s-put http://localhost:3030/example/data default test/turtle_example.ttl
 bin/s-query --service http://localhost:3030/example/query 'SELECT * {?s ?p ?o}'
 echo "----------"
 result=$(bin/s-query --service http://localhost:3030/example/query "SELECT * {<http://example.org/#green-goblin> <http://xmlns.com/foaf/0.1/name> ?o}")
-echo "result='$result'"
+echo "result s-query='$result'"
 echo "----------"
-echo $result | grep -o "Green Goblin"
-# echo HelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorld
-# docker exec $(docker ps -q) bin/s-put http://localhost:3030/example/data default test/turtle_example.ttl
-# ruby -v
-# ls -lat
-# ./test/sea_test.sh "aaa" "aaa" "string test words"
-# ./test/sea_test.sh "1" "1" "string test numbers"
-# ./test/sea_test.sh 1 1 "number test"
+result=$(echo $result | grep -o "Green Goblin")
+echo "result grep='$result'"
+echo "----------"
