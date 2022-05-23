491dc78a48faf948829913c545a950b6a5970201
Alain Helfesntein <alain.helfenstein@semantiker.ch>
2022-05-23 06:18:13 +0200

    load normal 30

--- test/base_test.sh
+++ test/base_test.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 # Test wether the string contains the pattern
+# parameters: string, pattern
 f_contains () {
-   echo "Test wether the two strings are equal '$1' = '$2'"
-   if [ "$1" == "$2" ]
+   echo "Test wether the strings contains the pattern '$1' = '$2'"
+   occurences=$(echo $1 | grep -c $2)
+   if [ $occurences >= 1 ]
    then
-     echo "test ok"
+     echo "test ok (occurences:$occurences)"
    else
-     echo "test not ok"
+     echo "test not ok\nthe string '$1' does not contain the pattern:'$2'"
      exit 1
    fi
 }
@@ -21,8 +23,5 @@ echo "----------"
 result=$(bin/s-query --service http://localhost:3030/example/query "SELECT * {<http://example.org/#green-goblin> <http://xmlns.com/foaf/0.1/name> ?o}")
 echo "result s-query='$result'"
 echo "----------"
-result=$(echo $result | grep -o "Green Goblin")
-echo "result grep='$result'"
-echo "----------"
 
-f_contains "Green Goblin" $result
+f_contains $result "Green Goblin"
