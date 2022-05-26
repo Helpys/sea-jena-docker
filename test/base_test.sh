15235405ce804927ef7593b966893289a00960d8
Alain Helfesntein <alain.helfenstein@semantiker.ch>
2022-05-26 11:51:21 +0200

    added test 22

--- test/base_test.sh
+++ test/base_test.sh
@@ -4,12 +4,14 @@
 
 echo "-------------------------------------------------------------------------------"
 echo "test file: ($0)"
-cat ./test/turtle_example.ttl | assert_contains "w2ww"
-echo "-------------------------------------------------------------------------------"
+cat ./test/turtle_example.ttl | assert_contains "www"
 
-# bin/s-put http://localhost:3030/example/data default test/turtle_example.ttl
-# bin/s-query --service http://localhost:3030/example/query 'SELECT * {?s ?p ?o}'
-# echo "----------"
+echo "-------------------------------------------------------------------------------"
+bin/s-put http://localhost:3030/example/data default test/turtle_example.ttl
+result=$(bin/s-query --service http://localhost:3030/example/query 'SELECT * {?s ?p ?o}')
+echo $result | assert_contains "Spiderman"
+
+# echo "-------------------------------------------------------------------------------"
 # result=$(bin/s-query --service http://localhost:3030/example/query "SELECT * {<http://example.org/#green-goblin> <http://xmlns.com/foaf/0.1/name> ?o}")
 # echo "result s-query='$result'"
 # echo "----------"
