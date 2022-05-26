64ad2b0f351175cac8616381dfe56367bf86ff58
Alain Helfesntein <alain.helfenstein@semantiker.ch>
2022-05-26 22:11:40 +0200

    assert color 2

--- test/fuseki_test.sh
+++ test/fuseki_test.sh
@@ -6,7 +6,9 @@ echo "test file: ($0)"
 
 echo "-------------------------------------------------------------------------------"
 bin/s-put http://localhost:3030/example/data default test/rdf_star_example.ttl
-sparql="PREFIX : <http://www.example.org/>
+sparql="BASE <http://www.example.org/>
+PREFIX : <http://example.org/>
+
 SELECT ?claimer WHERE {
    << :employee38 ?property ?value >> :accordingTo ?claimer
 }"
