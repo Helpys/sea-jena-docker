149132ef4da150caf0c014e0fce665296b8b126b
Alain Helfesntein <alain.helfenstein@semantiker.ch>
2022-05-26 11:45:53 +0200

    added test 21

--- testutil.sh
+++ testutil.sh
@@ -1,30 +1,18 @@
 #!/bin/sh
 docker ps -a
-docker exec $(docker ps -q) test/base_test.sh | echo "HELLO DOCKER"
-echo "last commands exit code:'echo $?'"
-docker exec $(docker ps -q) test/fuseki_test.sh | echo "last commands exit code:'echo $?'"
-echo "last commands exit code:'echo $?'"
+
+rm -f .temp
+docker exec $(docker ps -q) test/base_test.sh >> .temp
+docker exec $(docker ps -q) test/fuseki_test.sh >> .temp
+
+cat .temp
 
 
-
-# rm -f .temp
-#
-# echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX01 >> .temp
-# docker exec $(docker ps -q) test/base_test.sh >> .temp
-# docker exec $(docker ps -q) test/fuseki_test.sh >> .temp
-# echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX02 >> .temp
-#
-# cat .temp
-#
-# echo "ruby testutil.sh"
-# docker exec $(docker ps -q) ruby -v
-#
-# echo "x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x"
-# numberOfFails=$(grep "test fail" .temp | wc -l)
-# if [ $numberOfFails = 0 ]
-# then
-#   echo "Test OK"
-# else
-#   echo "$numberOfFails tests fails"
-#   exit 1
-# fi
+numberOfFails=$(grep "assertion-fail" .temp | wc -l)
+if [ $numberOfFails = 0 ]
+then
+  echo "all tests OK"
+else
+  echo "$numberOfFails tests fails"
+  exit 1
+fi
