242400c91bfa5bc717de469f450b2ac31cb7dbdd
Alain Helfesntein <alain.helfenstein@semantiker.ch>
2022-05-26 22:37:56 +0200

    sea example 2

--- test/sea_test.sh
+++ test/sea_test.sh
@@ -8,9 +8,11 @@ echo "--------------------------------------------------------------------------
 bin/s-put http://localhost:3030/example/data default test/sea_example.ttl
 sparql="BASE <http://www.example.org/>
 PREFIX : <http://example.org/>
+PREFIX seaa: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX xsd:  <http://www.w3.org/2001/XMLSchema#>
 
-SELECT ?claimer WHERE {
-   << :employee38 ?property ?value >> :accordingTo ?claimer
+SELECT ?decisionTime WHERE {
+   << :employee38 :familyName \"Smith\" >> seaa:decisionTime ?decisionTime
 }"
 result=$(bin/s-query --service http://localhost:3030/example/query "$sparql")
 echo $result | assert_contains "employee22"
