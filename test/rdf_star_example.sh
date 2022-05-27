1e6dc169eb9de22753cdfae5d3209956e5f96998
Alain Helfesntein <alain.helfenstein@semantiker.ch>
2022-05-27 08:14:52 +0200

    castBy

--- test/rdf_star_example.sh
+++ test/rdf_star_example.sh
@@ -14,3 +14,14 @@ SELECT ?claimer WHERE {
 }"
 result=$(bin/s-query --service http://localhost:3030/example/query "$sparql")
 echo $result | assert_contains "employee22"
+
+echo "-------------------------------------------------------------------------------"
+bin/s-put http://localhost:3030/example/data default test/rdf_star_example.ttl
+sparql="BASE <http://www.example.org/>
+PREFIX : <http://example.org/>
+
+SELECT ?link WHERE {
+   << :employee39 ?property ?value >> ?link ?claimer
+}"
+result=$(bin/s-query --service http://localhost:3030/example/query "$sparql")
+echo $result | assert_contains "castBy"
