b965786aea3fde4d44b68a9f0aeea3a6545f9703
Alain Helfesntein <alain.helfenstein@semantiker.ch>
2022-05-27 07:40:17 +0200

    sea transaction time

--- test/sea_test.sh
+++ test/sea_test.sh
@@ -16,3 +16,17 @@ SELECT ?decisionTime WHERE {
 }"
 result=$(bin/s-query --service http://localhost:3030/example/query "$sparql")
 echo $result | assert_contains "2020-01-01T17:07:07+02:00"
+
+echo "-------------------------------------------------------------------------------"
+bin/s-put http://localhost:3030/example/data default test/sea_example.ttl
+sparql="BASE <http://www.example.org/>
+PREFIX : <http://example.org/>
+PREFIX seaa: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX xsd:  <http://www.w3.org/2001/XMLSchema#>
+
+SELECT ?employee WHERE {
+   << ?employee :familyName "Smith" >> seaa:transactionTime ?transactionTime
+  FILTER(?transactionTime < NOW() )
+}"
+result=$(bin/s-query --service http://localhost:3030/example/query "$sparql")
+echo $result | assert_contains "employee11"
