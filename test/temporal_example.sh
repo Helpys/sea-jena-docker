2f90a955852ff7cc98f51b5ffe1a4138b4cf63f7
Alain Helfesntein <alain.helfenstein@semantiker.ch>
2022-05-28 21:53:25 +0200

    temporal 12

--- test/temporal_example.sh
+++ test/temporal_example.sh
@@ -44,3 +44,30 @@ SELECT ?eventId ?link ?value WHERE {
 }"
 result=$(bin/s-query --service http://localhost:3030/example/query "$sparql")
 echo $result | assert_contains "00000000-0000-0000-0000-000000000002"
+
+#-------------------------------------------------------------------------------------
+# find level by request- and intrest time 2021.07.11
+#-------------------------------------------------------------------------------------
+echo "-------------------------------------------------------------------------------"
+bin/s-put http://localhost:3030/example/data default test/temporal_example.ttl
+sparql="BASE <http://www.example.org/>
+PREFIX : <http://example.org/>
+PREFIX seaa: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX xsd:  <http://www.w3.org/2001/XMLSchema#>
+
+SELECT ?eventId ?level ?link ?value WHERE {
+   BIND(\"2021-07-11T00:00:00+02:00\"^^xsd:dateTime as ?requestTime)
+
+   << << :employee34 :level ?level >> seaa:eventId ?eventId >> seaa:deletionTime ?deletionTime .
+   FILTER(?deletionTime > ?requestTime)
+   << << :employee34 :level ?level >> seaa:eventId ?eventId >> seaa:transactionTime ?transactionTime .
+   FILTER(?transactionTime < ?requestTime)
+   << << :employee34 :level ?level >> seaa:eventId ?eventId >> seaa:validFrom ?validFrom .
+   FILTER(?validFrom < ?requestTime)
+   << << :employee34 :level ?level >> seaa:eventId ?eventId >> seaa:validTo ?validTo .
+   FILTER(?validTo > ?requestTime)
+
+   << << :employee34 :level ?level >> seaa:eventId ?eventId >> ?link ?value .
+}"
+result=$(bin/s-query --service http://localhost:3030/example/query "$sparql")
+echo $result | assert_contains "JUNIOR"
