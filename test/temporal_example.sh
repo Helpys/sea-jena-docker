f915eb3848515fcbc207b911c80f3325202c1edb
Alain Helfesntein <alain.helfenstein@semantiker.ch>
2022-05-28 21:25:44 +0200

    temporal 9

--- test/temporal_example.sh
+++ test/temporal_example.sh
@@ -4,6 +4,9 @@
 echo "-------------------------------------------------------------------------------"
 echo "test file: ($0)"
 
+#-------------------------------------------------------------------------------------
+# find latest Junior
+#-------------------------------------------------------------------------------------
 echo "-------------------------------------------------------------------------------"
 bin/s-put http://localhost:3030/example/data default test/temporal_example.ttl
 sparql="BASE <http://www.example.org/>
@@ -16,6 +19,25 @@ SELECT ?eventId ?link ?value WHERE {
    FILTER(?deletionTime > NOW())
 
    << << :employee34 :level \"Junior\" >> seaa:eventId ?eventId >> ?link ?value .
+}"
+result=$(bin/s-query --service http://localhost:3030/example/query "$sparql")
+echo $result | assert_contains "00000000-0000-0000-0000-000000000005"
+
+#-------------------------------------------------------------------------------------
+# find latest Expert
+#-------------------------------------------------------------------------------------
+echo "-------------------------------------------------------------------------------"
+bin/s-put http://localhost:3030/example/data default test/temporal_example.ttl
+sparql="BASE <http://www.example.org/>
+PREFIX : <http://example.org/>
+PREFIX seaa: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX xsd:  <http://www.w3.org/2001/XMLSchema#>
+
+SELECT ?eventId ?link ?value WHERE {
+   << << :employee34 :level \"Expert\" >> seaa:eventId ?eventId >> seaa:deletionTime ?deletionTime .
+   FILTER(?deletionTime > NOW())
+
+   << << :employee34 :level \"Expert\" >> seaa:eventId ?eventId >> ?link ?value .
 }"
 result=$(bin/s-query --service http://localhost:3030/example/query "$sparql")
 echo $result | assert_contains "00000000-0000-0000-0000-000000000005"
