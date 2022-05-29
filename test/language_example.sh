a5dab722aa791dea4fd7da517382242228fc9d14
Alain Helfesntein <alain.helfenstein@semantiker.ch>
2022-05-29 16:17:55 +0200

    load seaa-ontology 5

--- test/language_example.sh
+++ test/language_example.sh
@@ -15,8 +15,8 @@ PREFIX : <http://example.org/>
 PREFIX seaa: <http://www.seaa.ch/ontologies/2022/5/seaa-statement#>
 PREFIX xsd:  <http://www.w3.org/2001/XMLSchema#>
 
-SELECT ?name (lang(?name) as ?lang) WHERE {
-   << << :animal34 :name ?name >> seaa:eventId ?eventId >> seaa:transactionTime ?value .
+SELECT ?name ?key (lang(?name) as ?lang) WHERE {
+   << << ?e ?w ?name >> ?id ?eventId >> ?key ?value .
 }"
 result=$(bin/s-query --service http://localhost:3030/example/query "$sparql")
 echo $result
