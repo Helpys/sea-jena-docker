c60857d83b495784039e3dbd589324de33a74881
Alain Helfesntein <alain.helfenstein@semantiker.ch>
2022-06-01 07:35:59 +0200

    shacl 11

--- test/shacl_example.sh
+++ test/shacl_example.sh
@@ -12,7 +12,7 @@ bin/s-put http://localhost:3030/example/data default test/shacl_example.ttl
 
 result=$(curl -XPOST --data-binary @test/shacl_example.shacl.ttl  \
      --header 'Content-type: text/turtle' \
-     'http://localhost:3030/example/shacl?graph=default')
+     'http://localhost:3030/dataset/shacl?graph=default')
 
 echo "XXX-------------------------------------------------------------------------------"
 echo $result
