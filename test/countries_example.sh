f18d01dbfc3dae9aa09194e89cc207de90e16025
Alain Helfesntein <alain.helfenstein@semantiker.ch>
2022-06-29 16:51:16 +0200

    refactor bin 003

--- test/countries_example.sh
+++ test/countries_example.sh
@@ -12,6 +12,7 @@ apache_jena_script/s-delete "http://localhost:3030/example/data" "default"
 apache_jena_script/s-put http://localhost:3030/example/data default test/countries_data.ttl
 
 result=$(apache_jena_script/s-get http://localhost:3030/example/data default)
-echo $result
+echo "$result" | sed -r 's/[ü]/ü/g'
+
 echo $result | assert_contains "00000000-0000-0000-0020-100000000001"
 echo $result | assert_contains "00000000-0000-0000-0020-000000000001"
